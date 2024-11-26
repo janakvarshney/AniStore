25b01377d64f629bfa71aa85ccf14d68964b61cc
janak varshney <varshneyjanak@gmail.com>
2024-11-27 04:23:29 +0530

    changes basic ones

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,11 +1,21 @@
-import React,{useContext} from 'react'
+import React,{useContext, useState} from 'react'
 import { assets } from '../assets/assets';
 import { ShopContext } from '../context/ShopContext';
 
 const SearchBar = () => {
         const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
+        const location = useLocation() ;
+        const [visible,setVisible] = useState(false) ;
+        useEffect(()=>{
+                if(location.pathname.includes('collection') && showSearch){
+                        setVisible(true) ;
+                }
+                else{
+                        setVisible(false);
+                }
+        },[location])
 
-        return showSearch ? (
+        return showSearch && showSearch ? (
                 <div className='border-t border-b bg-gray-50 text-center'>
                         <div className='inline-flex justify-center items-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/4'>
                                 <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" placeholder='Search It!!' className='flex-1 outline-none bg-inherit text-sm' />
