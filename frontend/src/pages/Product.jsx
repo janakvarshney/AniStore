8abece6d32bf26510de2bd14261678835fceb2ee
janak varshney <varshneyjanak@gmail.com>
2024-11-28 07:57:23 +0530

    creating products seperate page part-1

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+
+  const { productID } = useParams();
+  // console.log(productID)
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image,setImage] = useState('') ;
+  const fetchProductData = async () =>{
+    products.map((item)=>{
+      if(item._id === productID){
+        setProductData(item)
+        setImage(item.image[0]) 
+        // console.log(item)
+        return null ;
+      }
+    })
+
+  }
+  useEffect(()=>{
+    fetchProductData() ;
+  },[productID,products]) ;
+
+  return productData ? (
+    <div className='border-t-2 transition-opacity ease-in duration-500 opacity-100'>
+      {/* productdata  */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row '>
+    {/* Products Images  */}
+      </div>
+
+    </div>
+  ) : <div className='opacity-0'> 
+  {/* if we do not get product data  */}
+  </div>
 }
 
 export default Product
