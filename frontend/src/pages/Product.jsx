1bc7caba74219ac05a2ed152d3bcb79e418d1b7e
janak varshney <varshneyjanak@gmail.com>
2024-12-24 14:25:35 +0530

    after a loong time ..resumed

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -8,32 +8,46 @@ const Product = () => {
   // console.log(productID)
   const { products } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
-  const [image,setImage] = useState('') ;
-  const fetchProductData = async () =>{
-    products.map((item)=>{
-      if(item._id === productID){
+  const [image, setImage] = useState('');
+  const fetchProductData = async () => {
+
+    products.map((item) => {
+      if (item._id === productID) {
         setProductData(item)
-        setImage(item.image[0]) 
-        // console.log(item)
-        return null ;
+        setImage(item.image[0])
+        console.log(item)
+        return null;
       }
     })
 
   }
-  useEffect(()=>{
-    fetchProductData() ;
-  },[productID,products]) ;
+  useEffect(() => {
+    fetchProductData();
+  }, [productID]);
 
   return productData ? (
     <div className='border-t-2 transition-opacity ease-in duration-500 opacity-100'>
       {/* productdata  */}
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row '>
-    {/* Products Images  */}
+        {/* Products Images  */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'> 
+
+          </div>
+          {
+            productData.image.map((item, index) => (
+              <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'/>
+            ))
+          }
+        </div>
+        <div className='w-full sm:w-[80%]'>
+          <img src={image} className='w-full h-auto' alt="" />
+        </div>
       </div>
 
     </div>
-  ) : <div className='opacity-0'> 
-  {/* if we do not get product data  */}
+  ) : <div className='opacity-0'>
+    {/* if we do not get product data  */}
   </div>
 }
 
