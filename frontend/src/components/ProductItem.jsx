138b653ce9916769cb2eb7f4589dc96f107846a2
janak varshney <varshneyjanak@gmail.com>
2024-12-27 11:03:58 +0530

    Cleared the bugs iin product.jsx and created the complete products.jsx and relateditem .jsx

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,25 +1,34 @@
-import React, { useContext } from "react";
-import { ShopContext } from "../context/ShopContext";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import { Link } from 'react-router-dom';
 
 const ProductItem = ({ id, image, name, price }) => {
   const { currency } = useContext(ShopContext);
+
   return (
-    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
-      <div className="overflow-hidden">
+    <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
+      <div className="overflow-hidden border rounded-lg p-4 shadow-sm  h-[300px]">
         <img
-          className="hover:scale-110 transition ease-in-out"
           src={image[0]}
           alt=""
+          className="w-full h-48 object-cover mb-2 rounded hover:scale-110 transition ease-in-out duration-500 "
         />
+        <p className="pt-3 pb-1 text-sm ">{name}</p>
+        <p className="text-sm font-medium">
+          {currency}
+          {price}
+        </p>
       </div>
-      <p className="pt-3 pb-1 text-sm">{name}</p>
-      <p className="text-sm font-medium">
-        {currency}
-        {price}
-      </p>
     </Link>
   );
 };
 
+// Add prop validation
+ProductItem.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  image: PropTypes.arrayOf(PropTypes.string),
+  price: PropTypes.number,
+};
 export default ProductItem;
