2107c2c215d7314a52fedc7778ea1b732fb6d33a
janak varshney <varshneyjanak@gmail.com>
2025-01-08 21:23:36 +0530

    changes in user controller...

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,56 @@
+import validator from 'validator';
+import userModel from '../models/userModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const createToken = (id) => {
+        return jwt.sign({ id }, process.env.JWT_SECRET);
+}
+
 //route for user login
 const userLogin = async (req, res) => {
-
 }
 
 
 //route for user registry
 const userRegistry = async (req, res) => {
-        res.json({msg:"User Registry working"});
+        try {
+                const { name, email, password } = req.body;
+                //checking if already exists or not
+                const exists = await userModel.findOne({ email });
+
+                if (exists) {
+                        return res.status(400).json({ success: false, message: "User already exists" });
+                }
+
+                //verifying email and strong password
+                if (!validator.isEmail(email)) {
+                        return res.status(400).json({ success: false, message: "Invalid email" });
+                }
+                if (password.length < 6) {
+                        return res.status(400).json({ success: false, message: "Password too short" });
+                }
+
+                //hashing user password
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(password, salt);
+
+                const newUser = new userModel({
+                        name,
+                        email,
+                        password: hashedPassword
+                })
+
+                const user = await newUser.save();
+
+                const token = createToken(user._id);
+                res.json({ success: true, token });
+
+
+        } catch (error) {
+                console.log(error);
+                res.json({ success: false, message: error.message });
+        }
 }
 
 //route for admin login 
@@ -15,4 +59,5 @@ const adminLogin = async (req, res) => {
 }
 
 
-export {userLogin, userRegistry, adminLogin};
+
+export { userLogin, userRegistry, adminLogin };
