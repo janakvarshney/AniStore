0f724fdaf7ac9ea5106e85a5a6a187b6b1c7145d
janak varshney <varshneyjanak@gmail.com>
2025-01-18 18:34:48 +0530

    warm up changes...,

--- admin/latest/src/App.jsx
+++ admin/latest/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
+import {Route,Routes} from 'react-router-dom'
+import Add from './pages/Add.jsx'
+import List from './pages/List.jsx'
+import Orders from './pages/Orders.jsx'
 
 
 const App = () => {
@@ -11,6 +15,13 @@ const App = () => {
         <hr />
         <div className='w-full flex'>
           <Sidebar />
+          <div className=' w-[82%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base ' > 
+          <Routes>
+            <Route path='/add' element={<Add />} />
+            <Route path='/list' element={<List/>} />
+            <Route path='/orders' element={<Orders />} />
+          </Routes>
+          </div>
         </div>
       </>
     </div>
--- admin/latest/src/components/Sidebar.jsx
+++ admin/latest/src/components/Sidebar.jsx
@@ -9,13 +9,13 @@ const Sidebar = () => {
                                 <img src={assets.add_icon} alt="icon" />
                                 <p className='hidden md:block'>Add Items</p>
                         </NavLink>
-                        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/add">
+                        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/list">
                                 <img src={assets.order_icon} alt="icon" />
                                 <p className='hidden md:block'>List Items</p>
                         </NavLink>
-                        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/add">
+                        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/orders">
                                 <img src={assets.order_icon} alt="icon" />
-                                <p className='hidden md:block'>Lists</p>
+                                <p className='hidden md:block'>Orders</p>
                         </NavLink>
                 </div>
                 </div>
